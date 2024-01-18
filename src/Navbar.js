3f17f3bd01c774af71af1c860bec0452db6858f2
Sharon Martin <the.painterly.girl@gmail.com>
2024-01-18 12:10:50 -0600

    Added Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,20 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom"
 
 function Navthing() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary mb-2">
       <Container>
-        <Navbar.Brand href="/home">Winnie the Pooh</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Winnie the Pooh</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/characters">Characters</Nav.Link>
-            <Nav.Link href="/quotes">Quotes</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/characters">Characters</Nav.Link>
+            <Nav.Link as={Link} to="/quotes">Quotes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
