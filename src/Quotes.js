11b6e1d6f208b65d95065a469e4cde46d9790b83
Sharon Martin <the.painterly.girl@gmail.com>
2024-01-18 14:54:56 -0600

    added responsiveness

--- src/Quotes.js
+++ src/Quotes.js
@@ -15,44 +15,41 @@ function Quotes() {
   }
 
   return (
-    <div className="d-flex justify-content-between pt-2">
-      <div style={{ width: "32vw" }}>
+    <div className="row pt-2">
+      <div className="d-flex flex-column align-items-center col-lg-6 col-12 order-lg-1 order-2">
         <img
-          style={{ width: "30vw" }}
-          src="../images/50554093697_efe400817f_k.jpg       "
+          className="w-75 pt-5"
+          // style={{ width: "30vw" }}
+          src="../images/50554093697_efe400817f_k.jpg"
         />
         <img
-          style={{ width: "35vw", paddingTop: "25px" }}
-          src="../images/50554093727_3d7b468344_k.jpg    "
+          className="w-100 pt-5"
+          // style={{ width: "35vw", paddingTop: "25px" }}
+          src="../images/50554093727_3d7b468344_k.jpg"
         />
       </div>
-
-      <div
-        style={{ width: "60vw" }}
-        className="d-flex flex-column justify-content-between"
-      >
-        <div className="d-flex justify-content-start">
-          <div className="p-4 border border-dark w-75">
+      <div className="col-lg-6 col-12 order-lg-2 order-1">
+        <div className="d-flex justify-content-lg-start justify-content-center">
+          <div className="p-4 border border-dark quoteContainer">
             <h2>Words of Wisdom from Pooh Bear and Friends</h2>
-            <div className="mx-5 my-2  quoteContainer">
-              "{quoteObject.quote}"
-            </div>
+            <div className="mx-lg-5 m-2">"{quoteObject.quote}"</div>
             <Button
               onClick={fetchQuote}
               variant="dark"
-              style={{ backgroundColor: "#6d4d36  " }}
+              style={{ backgroundColor: "#6d4d36" }}
             >
               Get New Quote
             </Button>
           </div>
         </div>
-        <div className="d-flex justify-content-end">
+        <div className="d-flex justify-content-lg-end justify-content-center">
           <img
-            style={{ width: "35vw" }}
+            className="width85"
+            style={{ paddingTop: "100px" }}
             src="../images/50554093672_29613e5e9e_k.jpg"
           />
         </div>
-      </div>
+      </div>{" "}
     </div>
   );
 }
